717afa58b5e495232b352321065fbd7f86faefd2
Vidyadhar <justvidyadhar@gmail.com>
2019-07-17 20:08:01 +0530

    Remove third image

--- instafilter.py
+++ instafilter.py
@@ -4,7 +4,6 @@ from PIL import Image
 # Load Images into Objects
 base_img = Image.open("portrait.jpg")
 img_filter = Image.open("kid.jpg")
-third = Image.open("art.jpg")
 
 # Set O/p Image Size
 size = (760,760)
@@ -12,14 +11,12 @@ size = (760,760)
 # Resize all images to o/p size
 base_img = base_img.resize(size)
 img_filter = img_filter.resize(size)
-third = third.resize(size)
 
 # Split each image into RGB vectors
 r, g, b = base_img.split()
 R,G,B = img_filter.split()
-Rr, Gg, Bb = third.split()
 
 # Merge RGB vectors from different images
-im = Image.merge("RGB", (r, Gg, B))
+im = Image.merge("RGB", (r, Gg, b))
 im.save("filtered.jpg", "JPG")
 
